6d924e7474adc95aa5d6c99fb218d44b23b409cb
SaberAlter <zzqchongya@foxmail.com>
2021-03-17 23:57:03 +0800

    feat: Object.create 继承问题

--- js/objectCreate.js
+++ js/objectCreate.js
@@ -1,7 +1,33 @@
-let a = {
-    a: 1,
-    b: 2
-}
-let b = Object.create(a)
-delete b.a
-console.log(b.a)
+(function () {
+        let a = {
+            a: 1,
+            b: 2
+        }
+        let b = Object.create(a)
+        delete b.a
+        console.log(b.a)
+    }
+)()
+
+;(function () {
+    class A {
+        constructor(name) {
+            this.name = name
+        }
+    }
+    class B extends A {
+        constructor(name, age) {
+            super(name)
+            this.age = age
+        }
+    }
+    let b = new B('ZhangSan', 18)
+    let c = Object.create(b)
+    let d = {}
+    d.__proto__ = b
+    console.log(c instanceof B, c instanceof A)
+    console.log(d.__proto__ === c.__proto__)
+})()
+
+// Object.create 是在原有的对象再继承下去
+// 理解为 {}.__proto__ = b
