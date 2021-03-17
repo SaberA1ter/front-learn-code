36e549542bff0e9e8c3112970cd7fb07d67fd364
SaberAlter <zzqchongya@foxmail.com>
2021-03-17 23:33:15 +0800

    feat: object 下的方法 call 改变 this 指向

--- js/functionInObject.js
+++ js/functionInObject.js
@@ -16,3 +16,10 @@ console.log(global.name)
  * 1、在 命令行 运行 node xxx.js 其 this 并不指向全局，var 并不会被挂载到 global 上
  * 2、在 浏览器 或者 cmd node 命令行，var 声明的变量是会被挂载到全局上
  */
+
+
+let anotherObj = {
+    name: 'XiaoHong',
+    age: 99
+}
+obj.fun.call(anotherObj)
