08c940e9045e6a134309c7648041052b3fb8a74f
SaberAlter <zzqchongya@foxmail.com>
2021-03-17 23:21:54 +0800

    feat: 对象中的this指向

---
+++ js/functionInObject.js
@@ -0,0 +1,18 @@
+var name = 'LiMing', age = 20
+var obj = {
+    name: this.name,
+    age: 18,
+    fun: function () {
+        console.log(this.name, this.age)
+    }
+}
+console.log(obj.name)
+obj.fun()
+
+console.log(global.name)
+
+/**
+ * 这段代码要注意
+ * 1、在 命令行 运行 node xxx.js 其 this 并不指向全局，var 并不会被挂载到 global 上
+ * 2、在 浏览器 或者 cmd node 命令行，var 声明的变量是会被挂载到全局上
+ */
